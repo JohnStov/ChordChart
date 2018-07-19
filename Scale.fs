c033a06601f794a4e4d4919f4659aacc46922411
John Stovin <john.d.stovin@gmail.com>
2018-07-19 14:28:56 +0100

    better types

--- Scale.fs
+++ Scale.fs
@@ -16,15 +16,13 @@ type Note =
 
 type Scale = Note list
 
-let chromatic = [C; Csharp; D; Eflat; E; F; Fsharp; G; Aflat; A; Bflat; B]
+let chromatic : Scale = [C; Csharp; D; Eflat; E; F; Fsharp; G; Aflat; A; Bflat; B]
 
-let major (scale : Note list) = 
+let major (scale : Scale) = 
     [ scale.[0]; scale.[2]; scale.[4]; scale.[5]; scale.[7]; scale.[9]; scale.[11] ]
 
-let minor (scale : Note list) = 
+let minor (scale : Scale) = 
     [ scale.[0]; scale.[2]; scale.[3]; scale.[5]; scale.[7]; scale.[8]; scale.[10] ]
 
-let scaleOf (note : Note) =
+let scaleOf (note : Note) : Scale =
     chromatic @ chromatic |> List.skipWhile (fun n -> n <> note) |> List.take 12
-
-let cChromatic = scaleOf C
