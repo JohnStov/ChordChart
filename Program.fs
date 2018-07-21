b9252ab9df2439a9d0310aa97d1266c6914423c8
John Stovin <john.d.stovin@gmail.com>
2018-07-21 12:35:14 +0100

    Notes in scale

--- Program.fs
+++ Program.fs
@@ -1,8 +1,10 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open Tab
+open Scale
 
 [<EntryPoint>]
 let main argv =
-    drawTab true 5 openD |> printf "%s"
+    let cMaj = octave C |> major
+    drawTab true 12 guitar cMaj |> printf "%s"
     0 // return an integer exit code
